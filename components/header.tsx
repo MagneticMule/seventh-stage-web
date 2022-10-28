17cdfa4dc570e50bf4bf768a8c62a5cb0af3dcce
Thomas Sweeney <skywriter@gmail.com>
2022-10-28 14:59:46 +0100

    feat: :sparkles: Added a link ro Goose

--- components/header.tsx
+++ components/header.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import BackgroundImage from '/images/header/header.svg';
 // import Whale from '/images/whale.png';
 import LogoImage from '/images/seventh_stage_logo.png';
+import Link from 'next/link';
 
 const HeaderContainer = styled.header`
   background-image: url(${BackgroundImage});
@@ -31,6 +32,13 @@ const HeaderContainer = styled.header`
     margin: 0 4rem;
     text-align: center;
     align-content: center;
+    margin-bottom: 1rem;
+  }
+
+  .highlight {
+    text-align: center;
+    align-content: center;
+    text-transform: uppercase;
   }
 `;
 
@@ -44,6 +52,9 @@ const Header = () => {
       <div className='blurb'>
         <h2>Shall be with you shortly.</h2>
         <a href='mailto:tommy@seventhstage.uk'>tommy@seventhstage.uk</a>
+      </div>
+      <div className='highlight'>
+        We just re-designed <a href='https://gooseashby.com'>Goose Clothing</a> why not have a gander?
       </div>
     </HeaderContainer>
   );
